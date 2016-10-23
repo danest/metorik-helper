26a085bdeb262092cd5764d71586f88a2aa98bd6
Bryce Adams <brycead@gmail.com>
2016-10-24 08:49:23 +1100

    Show notice after installing & 2.3
    Closes #1

--- metorik-helper.php
+++ metorik-helper.php
@@ -3,7 +3,7 @@
  * Plugin Name: Metorik Helper
  * Plugin URI: https://metorik.com
  * Description: Provides some fixes & extensions for WooCommerce, required by Metorik.
- * Version: 0.2.2
+ * Version: 0.2.3
  * Author: Metorik
  * Author URI: https://metorik.com
 */
@@ -21,6 +21,10 @@ class Metorik_Helper {
 	 */
 	public function init() {
 		if ( class_exists( 'WooCommerce' ) ) {
+			// Activate notice (shown once)
+			add_action( 'admin_notices', array( $this, 'activate_notice' ) );
+
+			// Require files for the plugin
 			require_once( 'inc/import.php' );
 			require_once( 'inc/api.php' );
 		} else {
@@ -32,7 +36,30 @@ class Metorik_Helper {
 	 * No WC notice.
 	 */
 	public function no_wc() {
-		echo '<div class="error"><p>' . sprintf( __( 'Metorik Helper requires %s to be installed and active.', 'metorik-helper' ), '<a href="https://woocommerce.com/" target="_blank">WooCommerce</a>' ) . '</p></div>';
+		echo '<div class="notice notice-error"><p>' . sprintf( __( 'Metorik Helper requires %s to be installed and active.', 'metorik-helper' ), '<a href="https://woocommerce.com/" target="_blank">WooCommerce</a>' ) . '</p></div>';
+	}
+
+	/**
+	 * Run on activation.
+	 */
+	public function activate() {
+		// Set Metorik's show activation notice option to true
+		update_option( 'metorik_show_activation_notice', true );
+	}
+
+	/**
+	 * Activate notice (if we should).
+	 */
+	public function activate_notice() {
+		if ( get_option( 'metorik_show_activation_notice', false ) ) {
+			echo '<div class="notice notice-success"><p>' . sprintf( __( 'The Metorik Helper is active! Go back to %s to complete the connection.', 'metorik-helper' ), '<a href="https://app.metorik.com/" target="_blank">Metorik</a>' ) . '</p></div>';
+
+			// Disable notice option
+			update_option( 'metorik_show_activation_notice', false );
+		}
 	}
 }
 new Metorik_Helper();
+
+// Notice after it's been activated
+register_activation_hook( __FILE__, array( 'Metorik_Helper', 'activate' ) );
--- readme.txt
+++ readme.txt
@@ -40,6 +40,9 @@ Keep in mind that you do need a Metorik account for it to work with, so if you d
 Yes, you do ([sign up here](https://metorik.com)). It will still work but will really not be of much use to you without one.
 
 == Changelog ==
+= 0.2.3 =
+* Show notice prompting users to go back to Metorik after installing to complete connection
+
 = 0.2.2 =
 * Fix customer IDs endpoint permissions
 
