f6eb8dd13035342dfe66f823330cc7ad9626e904
Bryce Adams <brycead@gmail.com>
2017-01-05 22:54:10 +1100

    Comment re: customers api endpoint

--- inc/api/customers.php
+++ inc/api/customers.php
@@ -16,7 +16,8 @@ class Metorik_Helper_API_Customers extends WC_REST_Posts_Controller {
 		add_action( 'rest_api_init', array( $this, 'customers_ids_route' ) );
 		add_action( 'rest_api_init', array( $this, 'customers_updated_route' ) );
 
-		// Temporarily override WC core customers/customer endpoints to fix bug that will be fixed in future WC release
+		// Temporarily override WC core customers/customer endpoints to fix bug with customer last order
+		// Don't need to do in 2.7 as just total spent / order count and we d that with meta filter
 		// This only happens during imports & for 2.6
 		if ( get_option( 'metorik_importing_currently', false ) && version_compare( WC()->version, '2.7.0', '<' ) ) {
 			$this->WC_REST_Customers_Controller = new WC_REST_Customers_Controller();
