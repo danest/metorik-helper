dbee9d5dd00c7f91ddbd45c0b9b5c93f96ace03c
Bryce Adams <bryce@metorik.com>
2018-03-28 22:55:20 +1100

    Add back version to Metorik Helper class

--- metorik-helper.php
+++ metorik-helper.php
@@ -3,14 +3,19 @@
  * Plugin Name: Metorik Helper
  * Plugin URI: https://metorik.com
  * Description: Provides some fixes & extensions for WooCommerce, required by Metorik.
- * Version: 0.14.1
+ * Version: 0.14.2
  * Author: Metorik
  * Author URI: https://metorik.com
  * WC requires at least: 2.6.0
  * WC tested up to: 3.3.0
 */
 
-class Metorik_Helper {
+class Metorik_Helper {	
+	/** 	
+	 * Current version of Metorik.	
+	 */	
+	public $version = '0.14.2';
+
 	/**
 	 * URL dir for plugin.
 	 */
