36c1033da5ec3e3afe70857f08f7f3655b66e338
Bryce Adams <brycead@gmail.com>
2019-04-20 01:20:09 +0000

    Apply fixes from StyleCI

--- inc/carts.php
+++ inc/carts.php
@@ -49,30 +49,31 @@ class Metorik_Helper_Carts
      * Check any prerequisites required for our add to cart request.
      * From https://barn2.co.uk/managing-cart-rest-api-woocommerce-3-6/.
      */
-    private function check_prerequisites() {
-        if ( defined( 'WC_ABSPATH' ) ) {
+    private function check_prerequisites()
+    {
+        if (defined('WC_ABSPATH')) {
             // WC 3.6+ - Cart and notice functions are not included during a REST request.
-            include_once WC_ABSPATH . 'includes/wc-cart-functions.php';
-            include_once WC_ABSPATH . 'includes/wc-notice-functions.php';
+            include_once WC_ABSPATH.'includes/wc-cart-functions.php';
+            include_once WC_ABSPATH.'includes/wc-notice-functions.php';
         }
 
-        if ( null === WC()->session ) {
-            $session_class = apply_filters( 'woocommerce_session_handler', 'WC_Session_Handler' );
+        if (null === WC()->session) {
+            $session_class = apply_filters('woocommerce_session_handler', 'WC_Session_Handler');
 
             //Prefix session class with global namespace if not already namespaced
-            if ( false === strpos( $session_class, '\\' ) ) {
-                $session_class = '\\' . $session_class;
+            if (false === strpos($session_class, '\\')) {
+                $session_class = '\\'.$session_class;
             }
 
             WC()->session = new $session_class();
             WC()->session->init();
         }
 
-        if ( null === WC()->customer ) {
-            WC()->customer = new \WC_Customer( get_current_user_id(), true );
+        if (null === WC()->customer) {
+            WC()->customer = new \WC_Customer(get_current_user_id(), true);
         }
 
-        if ( null === WC()->cart ) {
+        if (null === WC()->cart) {
             WC()->cart = new \WC_Cart();
 
             // We need to force a refresh of the cart contents from session here (cart contents are normally refreshed on wp_loaded, which has already happened by this point).
